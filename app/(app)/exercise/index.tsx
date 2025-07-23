8ea08ac09664746de786a64a0c52dcaf7c29c97d
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2025-07-23 13:20:27 -0300

    add: interfaces done

--- app/(app)/exercise/index.tsx
+++ app/(app)/exercise/index.tsx
@@ -1,5 +1,5 @@
 import { VStack } from "@/components/ui/vstack";
-import { TouchableOpacity } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native";
 import { ArrowLeft } from "lucide-react-native";
 import { Icon } from "@/components/ui/icon";
 import { useLocalSearchParams, useRouter } from "expo-router";
@@ -8,7 +8,12 @@ import { Text } from "@/components/ui/text";
 import { RouteProps } from "../home";
 import { Heading } from "@/components/ui/heading";
 import { useEffect } from "react";
+import { Image } from "@/components/ui/image"
+import { Box } from "@/components/ui/box";
 import BodySvg from "@assets/images/body.svg";
+import SeriesSvg from "@assets/images/series.svg"
+import RepetitionSvg from "@assets/images/repetitions.svg"
+import { Button } from "@/components/mine/Button";
 
 export default function Exercise() {
 
@@ -43,6 +48,47 @@ export default function Exercise() {
                 </HStack>
 
             </VStack>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 32 }}
+            >
+                <VStack className="p-8">
+                    <Image
+                        source={{
+                            uri: "https://preview.redd.it/tem-diferen%C3%A7a-fazer-puxada-frente-ou-graviton-v0-rigllwi8z2ke1.jpg?width=1080&crop=smart&auto=webp&s=a06e0b16720edaeb438994ea9387195452263b39"
+                        }}
+                        alt="Exercise image"
+                        className="w-full h-80 mb-3 rounded-lg"
+                        resizeMode="cover"
+                    />
+
+                    <HStack className="bg-gray-600 rounded-md pb-4 px-4 items-center justify-around ">
+                        <HStack className="items-center justify-center mb-6 mt-5">
+                            <SeriesSvg />
+                            <Text className="text-gray-200 ml-2">3 séries</Text>
+                        </HStack>
+                        <HStack>
+                            <RepetitionSvg />
+                            <Text className="text-gray-200 ml-2">12 repetições</Text>
+                        </HStack>
+                    </HStack>
+                    <Button title="Marcar como realizado" />
+
+
+                    <HStack className="bg-gray-600 rounded-md pb-4 px-4 items-center justify-around ">
+                        <HStack className="items-center justify-center mb-6 mt-5">
+                            <SeriesSvg />
+                            <Text className="text-gray-200 ml-2">3 séries</Text>
+                        </HStack>
+                        <HStack>
+                            <RepetitionSvg />
+                            <Text className="text-gray-200 ml-2">12 repetições</Text>
+                        </HStack>
+                    </HStack>
+                    <Button title="Marcar como realizado" />
+                </VStack>
+            </ScrollView>
+
         </VStack>
     )
 }
--- components/mine/HistoryCard.tsx
+++ components/mine/HistoryCard.tsx
@@ -6,8 +6,8 @@ import { Text } from "@/components/ui/text";
 export function HistoryCard() {
     return (
         <HStack className="w-full px-5 py-4 mb-3 bg-gray-600 rounded-md items-center justify-between">
-            <VStack className="mr-5">
-                <Heading className="text-white text-md capitalize font-heading">
+            <VStack className="flex-1 mr-5">
+                <Heading className="text-white text-md capitalize font-heading" numberOfLines={1}>
                     Costas
                 </Heading>
                 <Text className="text-gray-100 text-lg" numberOfLines={1}>
